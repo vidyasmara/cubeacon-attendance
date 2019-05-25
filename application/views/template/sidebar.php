6d3457217b9215ec12c76925b30edf9e01525af1
dws16 <dauuuussss@gmail.com>
2019-05-25 23:49:15 +0700

    typo

--- application/views/template/sidebar.php
+++ application/views/template/sidebar.php
@@ -25,45 +25,39 @@
 
         <?php
         $role_id = $this->session->userdata('role_id');
-        $queryMenu = "SELECT `user_menu`.`id`,`menu`
-                    FROM `user_menu` JOIN `user_access_menu`
-                    ON `user_menu`.`id` = `user_access_menu`.`menu_id`
-                    WHERE `user_access_menu`.`role_id` = $role_id
-                    ORDER BY `user_access_menu`.`menu_id` ASC
+        $queryMenu = "SELECT `id`,`menu`
+                    FROM `user_menu`
                     ";
-        $menu = $this->db->query($queryMenu)->result_array();
+        $menu = $this->db->query($queryMenu)->row_array();
         ?>
 
-        <?php foreach ($menu as $m) : ?>
+        <div class="sidebar-heading">
+            <?= $menu['menu']; ?>
+        </div>
 
-            <div class="sidebar-heading">
-                <?= $m['menu']; ?>
-            </div>
-
-            <?php
-            $menuId = $m['id'];
-            $querySubMenu = "SELECT *
+        <?php
+        $menuId = $m['id'];
+        $querySubMenu = "SELECT *
                     FROM `user_sub_menu`
                     WHERE `menu_id` = $menuId
                     AND `is_active` = 1
                     ";
-            $SubMenu = $this->db->query($querySubMenu)->result_array();
-            ?>
+        $SubMenu = $this->db->query($querySubMenu)->result_array();
+        ?>
 
-            <?php foreach ($SubMenu as $sm) : ?>
-                <?php if ($title == $sm['title']) : ?>
-                    <li class="nav-item active">
-                    <?php else : ?>
-                    <li class="nav-item">
-                    <?php endif; ?>
-                    <a class="nav-link pb-0" href="<?= base_url($sm['url']); ?>">
-                        <i class="<?= $sm['icon']; ?>"></i>
-                        <span><?= $sm['title']; ?></span></a>
-                </li>
-            <?php endforeach; ?>
-            <!-- Divider -->
-            <hr class="sidebar-divider mt-3">
+        <?php foreach ($SubMenu as $sm) : ?>
+            <?php if ($title == $sm['title']) : ?>
+                <li class="nav-item active">
+                <?php else : ?>
+                <li class="nav-item">
+                <?php endif; ?>
+                <a class="nav-link pb-0" href="<?= base_url($sm['url']); ?>">
+                    <i class="<?= $sm['icon']; ?>"></i>
+                    <span><?= $sm['title']; ?></span></a>
+            </li>
         <?php endforeach; ?>
+        <!-- Divider -->
+        <hr class="sidebar-divider mt-3">
 
         <li class="nav-item">
             <a class="nav-link collapsed" href="#" data-toggle="modal" data-target="#logoutModal">
