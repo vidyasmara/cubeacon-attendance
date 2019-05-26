f4b47d54668d469b8fcee17d2199732835f5b4e5
dws16 <dauuuussss@gmail.com>
2019-05-26 23:35:02 +0700

    typo

--- application/controllers/Dosen.php
+++ application/controllers/Dosen.php
@@ -5,7 +5,7 @@ class Dosen extends CI_Controller
 {
     public function detailmatkul($kodematkul)
     {
-        $matkul = $this->db->get_where('tb_matakuliah', ['id_matkul' => $kodematkul]);
+        $matkul = $this->db->get_where('tb_matakuliah', ['id_matkul' => $kodematkul])->row_array();
         $data['title'] = 'Log Absensi Mata Kuliah ' . $matkul['nama_matkul'];
         $data['user'] = $this->db->get_where('tb_user', ['nrp' => $this->session->userdata('nrp')])->row_array();
         $data['absen'] = $this->db->get_where('tb_absen', ['id_matkul' => $kodematkul]);
